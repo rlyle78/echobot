5c28bf9ed6a421cb709d5e2f3ef968e964e190e3
Kevin Goltermann <kggoltermann@gmail.com>
2017-01-13 18:35:14 -0600

    Added publish and subscribe keys for pubnub

--- droneApp/index.js
+++ droneApp/index.js
@@ -1,7 +1,7 @@
 var pubnub = require("pubnub")({
     ssl           : true,  // <- enable TLS Tunneling over TCP 
-    publish_key   : "pub-c-9b38a227-9c98-42dd-af43-bd09a3eff137",
-    subscribe_key : "sub-c-6d9eb720-2f24-11e5-bda8-02ee2ddab7fe"
+    publish_key   : "ppub-c-5c099dc9-4fb8-41d4-a462-54224a4f47d7",
+    subscribe_key : "sub-c-78d941e8-d9b7-11e6-b9cf-02ee2ddab7fe"
 });
 
 var five = require("johnny-five");
--- droneApp/rollingspider.js
+++ droneApp/rollingspider.js
@@ -10,8 +10,8 @@
 
 var pubnub = require("pubnub")({
     ssl           : true,  // <- enable TLS Tunneling over TCP 
-    publish_key   : "pub-c-....-bd09a3eff137",
-    subscribe_key : "sub-c-....-02ee2ddab7fe"
+    publish_key   : "pub-c-5c099dc9-4fb8-41d4-a462-54224a4f47d7",
+    subscribe_key : "sub-c-78d941e8-d9b7-11e6-b9cf-02ee2ddab7fe"
 });
 
 var RollingSpider = require("rolling-spider");
